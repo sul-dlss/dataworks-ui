49bad83528198250cd2b2704cca6d5a203b9fe11
Huda Khan <hudakhan@stanford.edu>
2025-10-13 14:19:30 -0600

    adding view component version constant

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require 'view_component/version'
 
 module App
   class Application < Rails::Application
