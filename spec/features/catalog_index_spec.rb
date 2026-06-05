7f502197c992a380dae7cdb8b9e67b25022f463a
Cory Lown <corylown@stanford.edu>
2026-06-05 14:15:53 -0400

    Set the page title consistently

--- spec/features/catalog_index_spec.rb
+++ spec/features/catalog_index_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Catalog index' do
   it 'renders the search page with seeded data in facets' do
     visit '/'
-    expect(page).to have_title('Blacklight')
+    expect(page).to have_title('DataWorks Experimental Prototype')
     expect(page).to have_css('form.search-query-form')
     expect(page).to have_text('figshare')
     expect(page).to have_text('Public')
